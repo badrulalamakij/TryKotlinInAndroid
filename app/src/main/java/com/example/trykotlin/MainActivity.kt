2159459f3c8916ee3032a82d7c0228dcad23ec07
badrulalamakij <badrulalam.akij@gmail.com>
2023-01-24 14:56:52 +0600

    Initial commit at github from android studio

---
+++ app/src/main/java/com/example/trykotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.trykotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        var myString: String? = null
+
+        lateinit var myLateinit: String
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myLateinit = "InitializedMe"
+
+        Log.i("lateinitAndLazy", "lateinit vs lazy : " + myString + ", " + myLateinit)
+
+
+    }
+}
