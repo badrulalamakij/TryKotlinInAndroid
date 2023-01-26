fb1e49fc498f22ca7d16d08cd52a46ec634219e5
badrulalamakij <badrulalam.akij@gmail.com>
2023-01-26 08:50:03 +0600

    fetch the update and merge it

--- app/src/main/java/com/example/trykotlin/MainActivity.kt
+++ app/src/main/java/com/example/trykotlin/MainActivity.kt
@@ -22,5 +22,49 @@ class MainActivity : AppCompatActivity() {
         Log.i("lateinitAndLazy", "lateinit vs lazy : " + myString + ", " + myLateinit)
 
 
+        funcA(1)
+
+
+    }
+
+    private fun funcA(i: Int) {
+        when(i){
+            1-> {
+                Log.d("taskRunning", "Task1 in A is working...")
+                funcB(1)
+            }
+            2->{
+                Log.d("taskRunning", "Task2 in A is working...")
+                funcB(2)
+            }
+            3->{
+                Log.d("taskRunning", "Task3 in A is working...")
+                funcB(3)
+            }
+            4->{
+                Log.d("taskRunning", "Task4 in A is working End.")
+                funcB(4)
+            }
+        }
+    }
+
+    private fun funcB(i: Int) {
+        when(i){
+            1->{
+                Log.d("taskRunning", "task1 in B is working...")
+                funcA(2)
+            }
+            2->{
+                Log.d("taskRunning", "task2 in B is working...")
+                funcA(3)
+            }
+            3->{
+                Log.d("taskRunning", "task3 in B is working...")
+                funcA(4)
+            }
+            4->{
+                Log.d("taskRunning", "task4 in B is working End.")
+            }
+        }
     }
 }
